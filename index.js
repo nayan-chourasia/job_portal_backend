d4d8940f138e1aed5276aaa168bd0baa61aaf662
nayan-chourasia <nayanchourasia0410@gmail.com>
2024-12-03 18:34:46 +0530

    changed cors options

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOption ={
-    origin:'http://localhost:5173',
-    credentials:true
-}
-app.use(cors(corsOption));
+// const corsOption ={
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
+app.use(cors());
 
 
 const PORT = process.env.PORT || 3000;
